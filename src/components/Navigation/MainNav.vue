fe1e7a00abea115783cce20abfe95d7fee30d214
Josh <you@example.com>
2022-06-12 10:04:24 +0800

    把router應用在原項目上

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -4,9 +4,12 @@
       <div
         class="flex h-full px-8 mx-auto border-b border-solid flex-nowrap border-brand-gray-1"
       >
-        <a :href="url" class="flex items-center h-full text-xl">{{
-          company
-        }}</a>
+        <router-link
+          :to="{ name: 'Home' }"
+          class="flex items-center h-full text-xl"
+        >
+          JoJo Careers
+        </router-link>
         <nav class="h-full ml-12">
           <ul class="flex h-full p-0 m-0 list-none">
             <li
@@ -50,8 +53,6 @@ export default {
   },
   data() {
     return {
-      company: "JoJo Careers",
-      url: "https://careers.google.com",
       menuItems: [
         "Teams",
         "Locations",
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,9 +1,19 @@
 <template>
-  <h1 class="text-6xl font-bold">Job Results View</h1>
+  <div>
+    <h1 class="text-6xl font-bold">Job Results View</h1>
+    <button @click="goToHome">go to home page</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "JobResultsView",
+  methods: {
+    goToHome() {
+      // this.$router.push("/");
+      // this.$router.push({ path: "/" });
+      this.$router.push({ name: "Home" });
+    },
+  },
 };
 </script>
