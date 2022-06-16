3b5148b8d47d74380eb6d157fe3fa8b2ed84ae6a
Josh <you@example.com>
2022-06-16 10:49:49 +0800

    JobListings設計一頁顯示10份工作並測試

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -4,16 +4,32 @@ jest.mock("axios");
 import JobListings from "@/components/JobResults/JobListings.vue";
 
 describe("JobListings", () => {
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: "5",
+      ...queryParams,
+    },
+  });
+  const createConfig = ($route) => ({
+    global: {
+      mocks: {
+        $route,
+      },
+    },
+  });
   it("fetches jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
-    shallowMount(JobListings);
+    const $route = createRoute();
+    shallowMount(JobListings, createConfig($route));
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
-  it("creates a job listing for each received job", async () => {
+  it("creates a job listing for a maximum of 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-    const wrapper = shallowMount(JobListings);
+    const queryParams = { page: "1" };
+    const $route = createRoute(queryParams);
+    const wrapper = shallowMount(JobListings, createConfig($route));
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
   });
 });
