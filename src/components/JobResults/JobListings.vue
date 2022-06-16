60d6381ca55bf6cd7d05d4327d314c650c93e54e
Josh <you@example.com>
2022-06-16 11:29:59 +0800

    JobResults分頁切換功能

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -11,6 +11,20 @@
     <div class="mx-auto mt-8">
       <div class="flex flex-row flex-nowrap">
         <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Previous</router-link
+          >
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Next</router-link
+          >
+        </div>
       </div>
     </div>
   </main>
@@ -30,6 +44,16 @@ export default {
     currentPage() {
       const pageString = this.$route.query.page || "1";
       return Number.parseInt(pageString);
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = this.jobs.length / 10;
+      return nextPage <= maxPage ? nextPage : undefined;
     },
     displayedJobs() {
       const pageNumber = this.currentPage;
