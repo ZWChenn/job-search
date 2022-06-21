5a68f2deca961292c4513fe1752dd2cf33f95589
Josh <you@example.com>
2022-06-21 15:13:39 +0800

    action內完成異步操作並commit給RECEIVE_JOBS

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { createStore } from "vuex";
+import getJobs from "@/api/getJobs";
+
 export const LOGIN_USER = "LOGIN_USER";
 export const RECEIVE_JOBS = "RECEIVE_JOBS";
+export const FETCH_JOBS = "FETCH_JOBS";
+
 export const state = () => {
   return {
     isLoggedIn: false,
@@ -15,9 +19,17 @@ export const mutations = {
     state.jobs = jobs;
   },
 };
+export const actions = {
+  [FETCH_JOBS]: async (context) => {
+    const jobListings = await getJobs();
+    context.commit(RECEIVE_JOBS, jobListings);
+  },
+};
+
 const store = createStore({
   state,
   mutations,
+  actions,
   strict: process.env.NODE_ENV !== "production",
 });
 
