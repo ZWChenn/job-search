7c8f06fa6f7386dd40390dfa8effe62b074e7a3b
Josh <you@example.com>
2022-06-29 09:09:59 +0800

    JobFiltersSidebarOrganizations中動態渲染UNIQUE_ORGANIZATIONS

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -3,21 +3,13 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="w-1/2 h-8">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <labe for="VueTube">VueTube</labe>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Between Vue and Me" type="checkbox" class="mr-3" />
-            <labe for="Between Vue and Me">Between Vue</labe>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-            <labe for="Et Vue Brute">Et Vue Brute</labe>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-            <labe for="Vue and a Half Men">Vue and a Half Men</labe>
+          <li
+            v-for="organization in UNIQUE_ORGANIZATIONS"
+            :key="organization"
+            class="w-1/2 h-8"
+          >
+            <input :id="organization" type="checkbox" class="mr-3" />
+            <labe :for="organization">{{ organization }}</labe>
           </li>
         </ul>
       </fieldset>
