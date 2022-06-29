229ba73fc7c259c2db2c86781fc1c20dabb635fe
Josh <you@example.com>
2022-06-29 15:07:04 +0800

    v-model綁定data跟checkbox

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -8,7 +8,13 @@
             :key="organization"
             class="w-1/2 h-8"
           >
-            <input :id="organization" type="checkbox" class="mr-3" />
+            <input
+              :id="organization"
+              v-model="selectedOrganizations"
+              :value="organization"
+              type="checkbox"
+              class="mr-3"
+            />
             <labe :for="organization" data-test="organization">{{
               organization
             }}</labe>
@@ -26,6 +32,11 @@ export default {
   name: "JobFiltersSidebarOrganizations",
   components: {
     Accordion,
+  },
+  data() {
+    return {
+      selectedOrganizations: [],
+    };
   },
   computed: {
     ...mapGetters([UNIQUE_ORGANIZATIONS]),
